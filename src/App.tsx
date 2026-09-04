20b29214b8ec968a445e918611f15d5ba04aadda
PRINCE CEEJAY NG <92728956+PRINCE-CEEJAY@users.noreply.github.com>
2026-09-04 14:48:15 +0000

    added notfou refactored and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import NotFound from './components/NotFound';
+import Homepage from './pages/Homepage';
+import NotFound from './pages/NotFound';
 export default function App() {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen'>
